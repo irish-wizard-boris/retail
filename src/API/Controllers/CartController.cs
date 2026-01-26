526900a37438c9f08eebb5638662cd2d375b57ea
unknown <boris22@protonmail.com>
2026-01-26 21:15:38 +0100

    Main solution + initial cart module structure

---
+++ src/API/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RetailAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CartController : ControllerBase
+    {
+
+    }
+}
